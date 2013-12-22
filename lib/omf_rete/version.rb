fdc0dd02637bbb8c8e8b8af8bcfe64c7a23a58c5
Max Ott <max.ott@nicta.com.au>
2013-12-23 10:42:10 +1100

    Increment version

--- lib/omf_rete/version.rb
+++ lib/omf_rete/version.rb
@@ -1,7 +1,7 @@
 
 module OMF
   module Rete
-    VERSION = '0.6'
+    VERSION = '0.6.1'
     # Used for finding the example directory
     TOP_DIR = File.dirname(File.dirname(File.dirname(__FILE__)))
   end
