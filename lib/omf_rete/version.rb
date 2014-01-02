7d5d169b629b6746250449ff9db5ca9b2a157b8b
Max Ott <max.ott@nicta.com.au>
2014-01-02 16:14:06 +1100

    Push out a new release

--- lib/omf_rete/version.rb
+++ lib/omf_rete/version.rb
@@ -1,7 +1,7 @@
 
 module OMF
   module Rete
-    VERSION = '0.6.1'
+    VERSION = '0.6.2'
     # Used for finding the example directory
     TOP_DIR = File.dirname(File.dirname(File.dirname(__FILE__)))
   end
